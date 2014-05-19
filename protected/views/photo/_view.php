404b873c701e0c49f436f0ebd33d7799fd32df06
Jyrone Parker <jyrone.parker@gmail.com>
2014-05-19 12:13:41 -0400

    Users now only see photos they uploaded on actionIndex page

--- protected/views/photo/_view.php
+++ protected/views/photo/_view.php
@@ -8,18 +8,17 @@
 
 
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('name')); ?>:</b>
-	<?php echo CHtml::encode($data->name); ?>
+	<b><?php echo CHtml::encode($data->getAttributeLabel('album')); ?>:</b>
+	<?php echo CHtml::encode($data->album->name); ?>
 	<br />
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('upload_time')); ?>:</b>
-	<?php echo CHtml::encode($data->upload_time); ?>
+	<?php echo CHtml::encode(date('l jS \of F Y h:i:s A',$data->upload_time)); ?>
 	<br />
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('uri')); ?>:</b>
 	<?php
 	$image = $data->name;//substr($data->uri, -15);
-	 echo CHtml::image(Yii::app()->baseUrl.'/images/'.$image,''); ?>
+	 echo CHtml::image(Yii::app()->baseUrl.'/images/'.$image,'',array('width'=>150,'height'=>150)); ?>
 	<br />
 
 
